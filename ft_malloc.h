92cbd72e87769524a03f3e3da711b44d37e2fb77
Esteban LECLET <eleclet@e2r13p7.42.fr>
2017-07-08 16:22:03 +0200

    first push

---
+++ ft_malloc.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malloc.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eleclet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/08 14:12:44 by eleclet           #+#    #+#             */
+/*   Updated: 2017/07/08 14:31:45 by eleclet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MALLOC_H
+#define FT_MALLOC_H
+
+#include "libft/libft.h"
+
+void	*ft_malloc(size_t *size);
+void	t_free(void *ptr);
+void	*ft_realloc(void *ptr, size_t size);
+
+#endif
