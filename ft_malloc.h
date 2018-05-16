0997e6de14e8d7b4acf456175aa2cb079e14a020
Esteban LECLET <eleclet@e2r9p7.42.fr>
2018-05-16 21:07:32 +0200

    Norme ok

--- ft_malloc.h
+++ ft_malloc.h
@@ -6,51 +6,42 @@
 /*   By: eleclet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 14:12:44 by eleclet           #+#    #+#             */
-/*   Updated: 2018/05/02 19:58:32 by eleclet          ###   ########.fr       */
+/*   Updated: 2018/05/16 21:07:02 by eleclet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MALLOC_H
-#define FT_MALLOC_H
+# define FT_MALLOC_H
 
-#include "libft/libft.h"
-#include <stdio.h>
-#include <sys/mman.h>
-#include <sys/resource.h>
+# include "libft/libft.h"
+# include <stdio.h>
+# include <sys/mman.h>
+# include <sys/resource.h>
 
-typedef		struct	s_zone
+typedef struct			s_zone
 {
-	int		type;
-	int		len;
-	int		state[100];
-	struct s_zone 	*prev;
-	struct s_zone 	*next;
-	void		*data;
-}			t_zone;
+	int					type;
+	int					len;
+	int					state[100];
+	struct s_zone		*prev;
+	struct s_zone		*next;
+	void				*data;
+}						t_zone;
 
-typedef		struct	s_block
-{
-	int		len;
-	
-}			t_block;
+void					*ft_malloc(size_t size);
+void					ft_free(void *ptr);
+void					*ft_realloc(void *ptr, size_t size);
 
-void	*ft_malloc(size_t size);
-void	ft_free(void *ptr);
-void	*ft_realloc(void *ptr, size_t size);
+void					*getmem(size_t size);
 
-void	*getmem(size_t size);
+t_zone					*gen_init();
 
-
-t_zone	*gen_init();
-
-int	find_type(size_t size);
-int	type_to_size(char type);
-void	*find_storage(t_zone *zone);
-void	*find_zone(char type, t_zone *gen);
-void	*create_zone(t_zone *gen, char type, size_t size);
-void	show_mem(t_zone *gen);
-void	print_state(t_zone *zone);
-t_zone	*getstatic(void);
-
-
+int						find_type(size_t size);
+int						type_to_size(char type);
+void					*find_storage(t_zone *zone);
+void					*find_zone(char type, t_zone *gen);
+void					*create_zone(t_zone *gen, char type, size_t size);
+void					show_mem(t_zone *gen);
+void					print_state(t_zone *zone);
+t_zone					*getstatic(void);
 #endif
--- gen_init.c
+++ gen_init.c
@@ -1,15 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gen_init.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eleclet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/16 19:55:17 by eleclet           #+#    #+#             */
+/*   Updated: 2018/05/16 19:55:23 by eleclet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_malloc.h"
 
-t_zone		*gen_init()
+t_zone		*gen_init(void)
 {
 	t_zone *tmp;
 
 	if ((tmp = getmem(sizeof(t_zone))) == NULL)
-		return NULL;
-	
-	tmp->prev	= NULL;
-	tmp->next	= NULL;
-	tmp->type	= 0;
-	
+		return (NULL);
+	tmp->prev = NULL;
+	tmp->next = NULL;
+	tmp->type = 0;
 	return (tmp);
 }
